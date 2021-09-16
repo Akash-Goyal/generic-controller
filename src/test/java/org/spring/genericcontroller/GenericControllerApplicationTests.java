577b97d6a1d4813b6a3b7525602915cb324aa898
Akash Goyal <ugotzenith@gmail.com>
2021-09-16 16:10:08 +0530

    Spring Project

---
+++ src/test/java/org/spring/genericcontroller/GenericControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.genericcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
